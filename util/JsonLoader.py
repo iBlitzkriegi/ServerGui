9330e4c2103749dcae6af091af856bd1fd56bd9f
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-09-28 23:34:53 -0400

    Implement setters for all attributes of a server

--- util/JsonLoader.py
+++ util/JsonLoader.py
@@ -6,6 +6,8 @@ class JsonHandler:
         with open('data.json', 'r') as file:
             self.data = json.load(file)
             file.close()
+        last_server = self.data['last-server']
+        self.last_server = self.data['servers'][last_server]
 
     def update_key(self, key, new_dictionary):
         self.data[key] = new_dictionary
@@ -21,5 +23,34 @@ class JsonHandler:
             self.dump_data()
             return
         last_server = self.data['last-server']
-        print(self.data['servers'][last_server])
-        return self.data['last-server']
+        return self.data['servers'][last_server]
+
+    def set_min_ram(self, ram):
+        self.last_server['min-ram'] = ram
+        self.dump_data()
+
+    def set_max_ram(self, ram):
+        self.last_server['max-ram'] = ram
+        self.dump_data()
+
+    def set_java_version(self, java_version):
+        self.last_server['java-version'] = java_version
+        self.dump_data()
+
+    def set_jar_file(self, path):
+        self.last_server['jar-file'] = path
+        self.dump_data()
+
+    def set_arguments(self, *args):
+        self.last_server['custom-arguments'] = args
+        self.dump_data()
+
+    def get_directory(self):
+        directory = self.last_server['jar-file']
+        path = ''.join([dirt + "/" for dirt in directory.split('\\') if '.jar' not in dirt])
+        data = {
+            "working-directory": path,
+            "full-path": [directory]
+        }
+        return data
+
